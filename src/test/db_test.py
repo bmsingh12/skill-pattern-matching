e980e580b88711ee162f659e4615a76842e89f10
infoTmt-USER <biraj.singh@infotmt.com>
2020-01-08 18:14:12 +0545

    changes to logic; updates skills now;

--- src/test/db_test.py
+++ src/test/db_test.py
@@ -23,10 +23,18 @@ rec = cursor.fetchall()
 # pprint.pprint(rec)
 
 
-# Update to old records
-def updateToOldSkills(id, skill):
-    psql_update_query = """ UPDATE sandbox12.skill SET name = %s WHERE id = %s """
+# Update records
+def updateSkills(id, skill):
+    psql_update_query = """ UPDATE skill_testing.skill SET name = %s WHERE id = %s """
     cursor.execute(psql_update_query, (skill, id))
+    connection.commit()
+
+
+# Insert to new table (testing table)
+def insertToTestingTable(id, name, type):
+    psql_insert_query = """ INSERT into skill_testing.skill(id, name, type) VALUES (%s,%s,%s) """
+    rec_to_insert = (id, name, type)
+    cursor.execute(psql_insert_query, rec_to_insert)
     connection.commit()
 
 
@@ -54,7 +62,11 @@ zip_data = [(x, y) for x, y in zip(list_id, skill_from_db())]
 
 # this will update to old skills w.r.to id
 # for x in zip_data:
-#     updateToOldSkills(x[0], x[1])
+#     updateSkills(x[0], x[1])
+
+#  this will insert data to new testing table (NOTE: use only after table is empty)
+# for id, name, type in rec:
+#     insertToTestingTable(id, name, type)
 
 ######################################################################################################
 ######################################################################################################
@@ -65,43 +77,39 @@ lfs = ['Oracle Java', 'Java', 'Oracle Java 2 Platform Enterprise Edition J2EE', 
 
 dic = {}
 lis = []
+d_t = {}
 
 for id, skill, type in rec:
     list_of_matching_skills = []
-    for skills_lookup in lfs:
+    tup = tuple()
+    for skills_lookup in lookupFileSkills():
         # print(skill)
         if fuzz.token_set_ratio(skill, skills_lookup) >= 90:
             list_of_matching_skills.append(skills_lookup)
             dic[skill] = list_of_matching_skills
             lis.append(dic)
+            tup = (id, skill, type)
+            d_t[tup] = list_of_matching_skills
 
-# print(lis[0])
-
-t = ("ff8081816ec604ae016ec63ad676001c", "sql", "hard")
-# d1 = {
-#     t: ['SQL', 'Oracle SQL Developer', 'Oracle SQL Loader', 'Oracle SQL Plus', 'T-SQL']
-# }
-d1 = {}
-d1[t] = ['SQL', 'Oracle SQL Developer', 'Oracle SQL Loader', 'Oracle SQL Plus', 'T-SQL']
-print(d1)
+# pprint.pprint(d_t)
 
 i = 1
 j = 1
 
-for k, v in lis[0].items():
+for k, v in d_t.items():
     print("\n")
-    print(i, "\b.", bcolors.BOLD + k + bcolors.ENDC, ":", v)
+    # print(i, "\b.", bcolors.BOLD + k[0] + bcolors.ENDC, ":", v)
+    print(i, "\b.", k, ":", v)
     i += 1
-    # if len(v) == 1:
-    #     print(bcolors.OKBLUE + "Only Match: " + bcolors.ENDC, v[0])
     seq_dict = {}
     for skill in v:
-        if k == skill.lower():
-            print(bcolors.OKGREEN + "Perfect Match: " + bcolors.ENDC, skill)
-            j += 1
-        else:
-            seq = SequenceMatcher(None, k.lower(), skill.lower())
-            seq_dict[skill] = seq.ratio()
+        # if k[1] == skill.lower():
+        #     print(bcolors.OKGREEN + "Perfect Match: " + bcolors.ENDC, skill)
+        #     j += 1
+        # else:
+        seq = SequenceMatcher(None, k[1].lower(), skill.lower())
+        seq_dict[skill] = seq.ratio()
     None if seq_dict == {} else print(bcolors.FAIL + "Max Value" + bcolors.ENDC,
                                       max(seq_dict, key=seq_dict.get), " : ", max(seq_dict.values()))
+    updateSkills(k[0], max(seq_dict, key=seq_dict.get))
     pprint.pprint(seq_dict)
